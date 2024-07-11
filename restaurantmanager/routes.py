0693dab4a6ab1962a7298583893fff8d79525b34
Antonio Bruchi <antoniobruchi@hotmail.com>
2024-07-11 11:53:06 +0100

    Feat: Add base user authentication routes
    Added routes for home, login and register and relative templates

--- restaurantmanager/routes.py
+++ restaurantmanager/routes.py
@@ -3,4 +3,14 @@ from restaurantmanager import app, db
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
