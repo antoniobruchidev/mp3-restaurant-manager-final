01db6ca25de3a1dda9d086cc9d091ed10f35a4d5
Antonio Bruchi <antoniobruchi@hotmail.com>
2024-07-13 13:15:18 +0100

    Feat: Add web3interface
    Deployed smart contract from the first owner address. Added functions to check a given role for a given addres and grant a given role to a given address. Stored smart contract ABI in AccessManager.json and added the smart contract code for reference

---
+++ restaurantmanager/static/contracts/AccessManager.sol
@@ -0,0 +1,144 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+/// @custom:security-contact antoniobruchidev@gmail.com
+contract AccessManager is AccessControl {
+
+    mapping(bytes32 role => RoleData) private _roles;
+
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+    bytes32 public constant WAITER_ROLE = keccak256("WAITER_ROLE");
+    bytes32 public constant CHEF_ROLE = keccak256("CHEF_ROLE");
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+    bytes32 public constant USER_ROLE = keccak256("USER_ROLE");
+
+    constructor(address defaultAdmin) {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(OWNER_ROLE, defaultAdmin);
+        _grantRole(MANAGER_ROLE, defaultAdmin);
+        _setRoleAdmin(MANAGER_ROLE, OWNER_ROLE);
+        _grantRole(WAITER_ROLE, defaultAdmin);
+        _setRoleAdmin(CHEF_ROLE, MANAGER_ROLE);
+        _setRoleAdmin(WAITER_ROLE, MANAGER_ROLE);    
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IAccessControl).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev Returns `true` if `account` has been granted `role`.
+     */
+    function hasRole(bytes32 role, address account) public view virtual override returns (bool) {
+        return _roles[role].hasRole[account];
+    }
+
+    function getRoleAdmin(bytes32 role) public view virtual override returns (bytes32) {
+        return _roles[role].adminRole;
+    }
+
+    /**
+     * @dev Grants `role` to `account`.
+     *
+     * If `account` had not been already granted `role`, emits a {RoleGranted}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     *
+     * May emit a {RoleGranted} event.
+     */
+    function grantRole(bytes32 role, address account) public virtual override onlyRole(getRoleAdmin(role)) {
+        _grantRole(role, account);
+    }
+
+    /**
+     * @dev Revokes `role` from `account`.
+     *
+     * If `account` had been granted `role`, emits a {RoleRevoked} event.
+     *
+     * Requirements:
+     *
+     * - the caller must have ``role``'s admin role.
+     *
+     * May emit a {RoleRevoked} event.
+     */
+    function revokeRole(bytes32 role, address account) public virtual override onlyRole(getRoleAdmin(role)) {
+        _revokeRole(role, account);
+    }
+
+    /**
+     * @dev Revokes `role` from the calling account.
+     *
+     * Roles are often managed via {grantRole} and {revokeRole}: this function's
+     * purpose is to provide a mechanism for accounts to lose their privileges
+     * if they are compromised (such as when a trusted device is misplaced).
+     *
+     * If the calling account had been revoked `role`, emits a {RoleRevoked}
+     * event.
+     *
+     * Requirements:
+     *
+     * - the caller must be `callerConfirmation`.
+     *
+     * May emit a {RoleRevoked} event.
+     */
+    function renounceRole(bytes32 role, address callerConfirmation) public virtual override {
+        if (callerConfirmation != _msgSender()) {
+            revert AccessControlBadConfirmation();
+        }
+
+        _revokeRole(role, callerConfirmation);
+    }
+
+    /**
+     * @dev Sets `adminRole` as ``role``'s admin role.
+     *
+     * Emits a {RoleAdminChanged} event.
+     */
+    function _setRoleAdmin(bytes32 role, bytes32 adminRole) internal virtual override{
+        bytes32 previousAdminRole = getRoleAdmin(role);
+        _roles[role].adminRole = adminRole;
+        emit RoleAdminChanged(role, previousAdminRole, adminRole);
+    }
+
+    /**
+     * @dev Attempts to grant `role` to `account` and returns a boolean indicating if `role` was granted.
+     *
+     * Internal function without access restriction.
+     *
+     * May emit a {RoleGranted} event.
+     */
+    function _grantRole(bytes32 role, address account) internal virtual override returns (bool) {
+        if (!hasRole(role, account)) {
+            _roles[role].hasRole[account] = true;
+            emit RoleGranted(role, account, _msgSender());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @dev Reverts with an {AccessControlUnauthorizedAccount} error if `_msgSender()`
+     * is missing `role`. Overriding this function changes the behavior of the {onlyRole} modifier.
+     */
+    function _checkRole(bytes32 role) internal view virtual override {
+        _checkRole(role, _msgSender());
+    }
+
+    /**
+     * @dev Reverts with an {AccessControlUnauthorizedAccount} error if `account`
+     * is missing `role`.
+     */
+    function _checkRole(bytes32 role, address account) internal view virtual override {
+        if (!hasRole(role, account)) {
+            revert AccessControlUnauthorizedAccount(account, role);
+        }
+    }
+}
+
---
+++ restaurantmanager/web3interface.py
@@ -0,0 +1,95 @@
+import json
+import os
+from web3 import Web3
+from eth_account import Account
+from eth_account.signers.local import LocalAccount
+from web3.middleware import construct_sign_and_send_raw_middleware
+
+# create an instance of the web3 provider
+w3 = Web3(Web3.HTTPProvider(os.environ.get("ALCHEMY_KEY")))
+
+# check the connection
+print('Web3 connection: ', w3.is_connected())
+
+# check for a valid private_key
+private_key = os.environ.get("PRIVATE_KEY")
+assert private_key is not None, "You must set PRIVATE_KEY environment variable"
+assert private_key.startswith(
+    "0x"), "Private key must start with 0x hex prefix"
+
+# access a local account with the private_key
+account: LocalAccount = Account.from_key(private_key)
+w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
+
+# access the smart contract ABI
+with open('restaurantmanager/AccessManager.json') as f:
+    abi=json.load(f)
+
+# create an instance of the contract
+accessmanager = w3.eth.contract(address=os.environ.get("CONTRACT_ADDRESS"), abi=abi)
+
+# defining the functions we are going to call
+has_role_func = accessmanager.get_function_by_signature(
+    'hasRole(bytes32,address)')
+grant_role_func = accessmanager.get_function_by_signature(
+    'grantRole(bytes32,address)')
+get_role_admin = accessmanager.get_function_by_signature(
+    'getRoleAdmin(bytes32)')
+
+# function to check a given role for a given address
+def check_role(role, address):
+    # convert the address to a checksum address
+    cs_address = w3.to_checksum_address(address)
+    result = has_role_func(role,cs_address).call()
+    return result
+
+
+# function to grant a given role to a given address
+def grant_role(role, granting_address, granted_address):
+    # get the Admin role for the given role
+    roleAdmin = get_role_admin(role)
+    # convert addresses to checksum addresses
+    cs_granting_address = w3.to_checksum_address(granting_address)
+    cs_granted_address = w3.to_checksum_address(granted_address)
+    # check if the sender has an Admin role for the given role
+    isAdmin = has_role_func(roleAdmin, cs_granting_address)
+    if isAdmin:
+        # initialize the chain id, we need it to build the transaction for replay protection
+        Chain_id = w3.eth.chain_id
+        # call the function
+        nonce = w3.eth.get_transaction_count(account.address)
+        call_function = grant_role_func(
+            role,cs_granted_address
+            ).build_transaction({"chainId": Chain_id, "from": account.address, "nonce": nonce})
+        # Sign transaction
+        signed_tx = w3.eth.account.sign_transaction(call_function, private_key=private_key)
+        # Send transaction
+        send_tx = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+
+        # Wait for transaction receipt
+        tx_receipt = w3.eth.wait_for_transaction_receipt(send_tx)
+        # convert the transaction hash to hexadecimal
+        tx_hash = w3.to_hex(tx_receipt.transactionHash)
+        print(tx_hash)
+        if tx_receipt.status == 1:
+            return {"success": True, "tx_hash": tx_hash}
+        else:
+            return {"success": False, "tx_receipt": tx_receipt}
+    else:
+        return {"success": False, "error": 403}
+    
+
+def role_hash(role):
+    match role:
+        case "owner":
+            return "0xb19546dff01e856fb3f010c267a7b1c60363cf8a4664e21cc89c26224620214e"
+        case "manager":
+            return "0x241ecf16d79d0f8dbfb92cbc07fe17840425976cf0667f022fe9877caa831b08"
+        case "chef":
+            return "0xbb95cca64affbe55a6a6f6b690ae1c8525d24dc953087f6db9a21e7cb374b385"
+        case "waiter":
+            return "0xcb57d3fb43d2386a5b9956d50e3893564d63703e540d4377ea7d0ea153e406d8"
+        case "user":
+            return "0x14823911f2da1b49f045a0929a60b8c1f2a7fc8c06c7284ca3e8ab4e193a08c8"
+        case _:
+            return False
